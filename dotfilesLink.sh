a9d7d844b8b6d67967def5dd6c26029f07cff578
aikiyy <aiki.yougai@gmail.com>
2019-08-27 16:04:01 +0900

    add ide command for tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,6 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+ln -sf ~/dotfiles/commands/ide /usr/local/bin/ide
