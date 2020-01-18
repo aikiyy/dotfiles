48bf2b478ded43985a0134a50d6efb43a500962a
aikiyy <aiki.yougai@gmail.com>
2020-01-18 15:09:45 +0900

    Add .ipython .jupyter

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,5 +5,7 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.jupyter ~/.jupyter
+ln -sf ~/dotfiles/.ipython ~/.ipython
 
 ln -sf ~/dotfiles/commands/ide /usr/local/bin/ide
