b5afee197a3eb073a59b3bc77b959f1461fbbcbc
aikiyy <aikiyy@users.noreply.github.com>
2020-04-04 21:41:24 +0900

    Fix gitignore_global

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,8 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+mkdir -p ~/.config/git
+ln -sf ~/dotfiles/.gitignore_global ~/.config/git/ignore
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.jupyter ~/.jupyter
